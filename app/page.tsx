47d8d4687a840e44c88257e7405ceea681c6363c
Andrew Kim <ndbkim@gmail.com>
2025-09-18 21:56:03 -0700

    added fix for tab still saying 'create next app' instead of 'andy kim'

--- app/page.tsx
+++ app/page.tsx
@@ -5,36 +5,45 @@ export default function Home() {
   const x = 10
   const randomThing : string = "hi"
   return (
-    <div className={styles.root}>
-      <h1>ANDY KIM</h1> 
-      <p className={styles.subtitle1}>*********************************************************</p>
-      <p className={styles.subtitle1}>
-        welcome to my website!
-        my name is andrew (andy) kim. i am an ex-aerospace engineer-turned-software-developer/game-developer 
-        and am passionate about all things software! below are some links 
-        that will take you to some of my projects. enjoy!
-      </p>
-      <p className={styles.subtitle1}>*********************************************************</p>
-      <p className={styles.subtitle1}>links:</p>
-      <div className={styles.links}>
-        <a href="https://a-kimb0.itch.io/" target="_blank" rel="noopener noreferrer">
-          Itch.io |
-        </a>
-        <a href="https://github.com/akimb" target="_blank" rel="noopener noreferrer">
-          GitHub |
-        </a>
-        <a href="https://www.linkedin.com/in/andrewkim101/" target="_blank" rel="noopener noreferrer">
-          LinkedIn |
-        </a>
-        <a href="https://godotshaders.com/shader/futuristic-jet-thruster/" target="_blank" rel="noopener noreferrer">
-          Shaders |
-        </a>
-        <a href="Andrew Kim Technical Resume.pdf" target="_blank" rel="noopener noreferrer">
-          Resume
-        </a>
-      </div>
-      {/* <p className={styles.subtitle2}>github</p> */}
+    <>
+      <head>
+        <title>Andy Kim</title>
+        <meta name="description" content="Andy Kim's Personal Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="moodle_content_ui.png" />
+      </head>
 
-    </div>
+      <div className={styles.root}>
+        <h1>ANDY KIM</h1> 
+        <p className={styles.subtitle1}>*********************************************************</p>
+        <p className={styles.subtitle1}>
+          welcome to my website!
+          my name is andrew (andy) kim. i am an ex-aerospace engineer-turned-software-developer/game-developer 
+          and am passionate about all things software! below are some links 
+          that will take you to some of my projects. enjoy!
+        </p>
+        <p className={styles.subtitle1}>*********************************************************</p>
+        <p className={styles.subtitle1}>links:</p>
+        <div className={styles.links}>
+          <a href="https://a-kimb0.itch.io/" target="_blank" rel="noopener noreferrer">
+            Itch.io |
+          </a>
+          <a href="https://github.com/akimb" target="_blank" rel="noopener noreferrer">
+            GitHub |
+          </a>
+          <a href="https://www.linkedin.com/in/andrewkim101/" target="_blank" rel="noopener noreferrer">
+            LinkedIn |
+          </a>
+          <a href="https://godotshaders.com/shader/futuristic-jet-thruster/" target="_blank" rel="noopener noreferrer">
+            Shaders |
+          </a>
+          <a href="Andrew Kim Technical Resume.pdf" target="_blank" rel="noopener noreferrer">
+            Resume
+          </a>
+        </div>
+        {/* <p className={styles.subtitle2}>github</p> */}
+
+      </div>
+    </>
   );
 }
